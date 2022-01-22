90761f325ab9c7f9326cfd9a1900d577c0711c4e
joshg <joshgoodjob2@gmail.com>
2022-01-22 16:16:51 +0900

    week2 input,print,type,불린형,정수형,문자열?

---
+++ lol.py
@@ -0,0 +1,11 @@
+import turtle
+turtle.turtles
+turtle.forward(100)
+turtle.left(144)
+turtle.forward(100)
+turtle.left(144)
+turtle.forward(100)
+turtle.left(144)
+turtle.forward(100)
+turtle.left(144)
+turtle.forward(100)
---
+++ sim.py
@@ -0,0 +1,30 @@
+'''
+#way 1
+a = "apple"
+b = "banana"
+c = "cola"
+print("%s는 %s이고 %s이다"%(a, b, c))
+print(a, b, c)
+
+#way 2
+print(f"{a} 는 {b} 이고 {c} 이다")
+
+print("josh","이준서")
+print(a, b, c, a, b, c, sep= ' , ')
+'''
+
+#연습문제1
+input("이름을입력하세요.")
+print("%s님 안녕하세요" %(name))
+
+#연습문제
+name=input("이름을 입력하세요.")
+say=input("지금 하고싶은 말은?")
+print(f"{name}는 {say}")
+
+#히히
+#히히
+
+input("히")
+print("히")
+#이윤서 바보
